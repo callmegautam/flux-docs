661553aaacaf1306e3c74ce5001f2c0fc4d117df
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-06-16 11:11:42 +0530

    Update project configuration and dependencies
    - Add .vscode to .gitignore
    - Update import statements to use double quotes for consistency
    - Modify layout configuration to include navigation links
    - Update index.mdx to reflect project name
    - Add new dependencies in package.json and update pnpm-lock.yaml

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -8,21 +8,29 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
-  },
-  // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+    nav: {
+        title: (
+            <>
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
+                    <circle cx={12} cy={12} r={12} fill="currentColor" />
+                </svg>
+                Flux Docs
+            </>
+        ),
+        // transparentMode: "always",
+    },
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+        {
+            type: "menu",
+            text: "Guide",
+            items: [
+                {
+                    text: "Getting Started",
+                    description: "Learn to use Fumadocs",
+                    url: "/docs",
+                },
+            ],
+        },
+    ],
 };
