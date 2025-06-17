7634e6f307c1bbcb48b0ed9aa658f8467d4b15cf
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-06-17 09:15:00 +0530

    Add Flux logo image and update layout configuration to use it

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,5 +1,7 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
+import Image from "next/image";
+
 /**
  * Shared layout configurations
  *
@@ -11,9 +13,10 @@ export const baseOptions: BaseLayoutProps = {
     nav: {
         title: (
             <>
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
+                {/* <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
                     <circle cx={12} cy={12} r={12} fill="currentColor" />
-                </svg>
+                </svg> */}
+                <Image src="/flux-logo.png" alt="Logo" width={24} height={24} className="rounded-full" />
                 Flux Docs
             </>
         ),
